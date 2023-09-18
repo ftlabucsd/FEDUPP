b1b7b230cc2ca3577787484dd68898302e4457d3
AndyWeasley2004 <m5yao@ucsd.edu>
2023-09-18 09:54:36 -0700

    inital upload

---
+++ meals.py
@@ -0,0 +1,104 @@
+from cProfile import label
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.patches as mpatches
+from datetime import timedelta
+
+plt.rcParams['figure.figsize'] = (15, 6)
+
+
+def process_sheet(path: str, sheet: str) -> pd.DataFrame:
+    """Preprocess the excel sheet for analysis
+    keep only useful columns and rename improper named column
+    drop null value and convert time to datetime format
+    reset index to start with 0
+    """
+    df = pd.read_excel(path, sheet_name=sheet)
+    df = df[['MM:DD:YYYY hh:mm:ss', 'Event', 'Pellet_Count', 'Cum_Sum']].rename(
+        columns={'MM:DD:YYYY hh:mm:ss': 'Time'})
+    df.dropna(inplace=True)
+    df['Time'] = pd.to_datetime(df['Time'])
+    df = df.reset_index().drop(['index'], axis='columns')
+    return df
+
+def find_pellet_frequency(data: pd.DataFrame) -> pd.DataFrame:
+    """find number of pellet in every 10 minutes
+    return a new data frame records the 10 minutes pellet
+    """
+    data = data.drop(['Pellet_Count', 'Cum_Sum'], axis='columns')
+    data.set_index('Time', inplace=True)
+    grouped_data = data[data['Event'] == 'Pellet'].resample('10T').size().reset_index()
+    grouped_data.columns = ['Interval_Start', 'Pellet_Count']
+    
+    return grouped_data
+
+def graph_pellet_frequency(grouped_data: pd.DataFrame):
+    """graph histogram for pellet frequency
+    histogram analysis
+    """
+    plt.figure(figsize=(20, 6))
+    ax = sns.barplot(data=grouped_data, x='Interval_Start', y='Pellet_Count')
+
+    # Get the x-axis positions
+    xtick_positions = ax.get_xticks()
+
+    # Update x-axis labels to display one-hour intervals
+    hourly_labels = [label.strftime('%H:%M') for label in grouped_data['Interval_Start'] if label.minute == 0]
+    hourly_positions = [pos for pos, label in zip(xtick_positions, grouped_data['Interval_Start']) if label.minute == 0]
+
+    ax.set_xticks(hourly_positions)  # Set the tick positions to match the hourly intervals
+    ax.set_xticklabels(hourly_labels, rotation=45, horizontalalignment='right')  # Set the tick labels to hourly format
+    
+    plt.axhline(y=5, color='red', linestyle='--', label='meal')
+    plt.title('Pellet Frequency', fontsize=18)
+    plt.xlabel('Time', fontsize=14)
+    plt.ylabel('Number of Pellet', fontsize=14)
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+def find_meals(data: pd.DataFrame) -> list:
+    """
+    find meals in the behaviors. 5 pellets in 12 minutes is considered as a meal
+    """
+    meal_list = []
+    pellet_count_threshold = 5
+    window_duration = timedelta(minutes=12)
+    start_idx = 0
+
+    for idx, row in data.iterrows():
+        meal_start = data.loc[start_idx, 'Time']
+        time_diff = row['Time'] - meal_start
+
+        if (row['Pellet_Count'] - data.loc[start_idx, 'Pellet_Count'] >= 
+                    pellet_count_threshold) and (time_diff <= window_duration):
+            meal_list.append([meal_start, row['Time']])
+        elif time_diff > window_duration:
+            start_idx = idx
+
+    return meal_list
+
+
+def graphing_cum_count(data: pd.DataFrame, meal: list):
+    """graph the cumulative count and percentage of pellet consumption
+    use two axis and mark meals on the graph
+    """
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Time', fontsize=12)
+    ax1.set_ylabel('Pellet_Count', fontsize=12)
+    ax1.plot(data['Time'], data['Pellet_Count'], color='blue')
+
+    ax2 = ax1.twinx()  # Share the same x-axis as ax1
+    ax2.set_ylabel('Cum_Sum', fontsize=12)
+    ax2.plot(data['Time'], data['Cum_Sum'], color='blue')
+
+    ax1.set_title('Pellet Count and Cumulative Sum Over Time', fontsize=18)
+
+    for interval in meal:
+        plt.axvspan(interval[0], interval[1], color='lightblue', alpha=0.8)
+
+    patch = mpatches.Patch(color='lightblue', alpha=0.8, label='Meal')
+    plt.legend(handles=[patch], loc='upper left')
+    plt.show()
---
+++ tools.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import os
+
+
+def combine_csv(parent: str) -> pd.DataFrame:
+    # old version cleaning
+    output = os.path.join(parent, 'combined.csv')
+    if os.path.exists(output):
+        os.remove(output)
+
+    # process file list
+    files = os.listdir(path=parent)
+    files.remove('.DS_Store')
+    for i in range(len(files)):
+        files[i] = os.path.join(parent, files[i])
+
+    # concat csv files
+    result = pd.DataFrame()
+    length = []
+    for file in files:
+        curr = pd.read_csv(file)    # read current file
+
+        curr = curr[['MM:DD:YYYY hh:mm:ss', 'Event', 'Active_Poke']].rename(
+            {'MM:DD:YYYY hh:mm:ss': 'Time_Stamp'})
+
+        curr = curr.replace({'LeftWithPellet': 'Left', 'LeftDuringDispense': 'Left',
+                             'RightWithPellet': 'Right', 'RightDuringDispense': 'Right'})
+
+        length.append(curr.shape[0])
+        if result.empty:
+            result = curr
+        else:
+            result = pd.concat([result, curr], ignore_index=True)
+
+    print("Files have length", length)
+    print("Total length should be", sum(length))
+
+    # return and save results
+    result.to_csv(output)
+    return result
+
+
+def count_error_rate(data: pd.DataFrame) -> tuple:
+    error_R = 0
+    error_L = 0
+    total_R = 0
+    total_L = 0
+
+    for idx, row in data.iterrows():
+        active_poke = row['Active_Poke']
+        event = row['Event']
+
+        if active_poke == 'Left':   # left active
+            if event != 'Pellet':   # pellet and correct are overlapping
+                total_L += 1
+
+            if event == 'Right':
+                error_L += 1
+        else:       # right active
+            if event != 'Pellet':
+                total_R += 1
+
+            if event == 'Left':
+                error_R += 1
+
+    rateL = round(error_L / total_L, 2)
+    rateR = round(error_R / total_R, 2)
+
+    return rateL, rateR
+
+
+def preprocess_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+    data = data[['Event', 'Active_Poke']]
+
+    data = data.replace({'LeftWithPellet': 'Left', 'LeftDuringDispense': 'Left',
+                         'RightWithPellet': 'Right', 'RightDuringDispense': 'Right'})
+
+    return data
+
+
+def parent_directory_process(parent: str):
+    files = os.listdir(path=parent)
+    files.remove('.DS_Store')
+
+    for i in range(len(files)):
+        files[i] = os.path.join(parent, files[i])
+
+    return files
