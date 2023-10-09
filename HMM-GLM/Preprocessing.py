7c41acb5bd317c13817579d8be80e66982afea31
AndyWeasley2004 <m5yao@ucsd.edu>
2023-10-09 11:45:13 -0700

    Perform basic HMM model and graph the transition matrix

--- HMM-GLM/Preprocessing.py
+++ HMM-GLM/Preprocessing.py
@@ -1,18 +1,30 @@
 import pandas as pd
-
+import numpy as np
 
 def extract_features(path: str) -> tuple:
+    """
+    extract current active poke, previous choice, previous active poke as X
+    use current event as output
+    """
     data = pd.read_csv(path)
     data = data[['Event', 'Active_Poke']].replace({'LeftWithPellet': 'Left', 'LeftDuringDispense': 'Left',
                                         'RightWithPellet': 'Right', 'RightDuringDispense': 'Right'})
 
-    data = data[data['event'] != 'pellet']
+    data = data[data['Event'] != 'Pellet']
     data['prev_event'] = data['Event'].shift(fill_value=None)
     data['prev_active'] = data['Active_Poke'].shift(fill_value=None)
-    # TODO: encode left and right with 0 and 1
     if not data.empty:
         data = data.iloc[1:]
 
     X = data.drop(['Event'], axis='columns')
     y = data['Event']
-    return X.values.tolist(), y.values.tolist()
+
+    mapper = {'Left': 0, 'Right': 1, 'Pellet': 2}
+    X['Active_Poke'] = X['Active_Poke'].map(mapper)
+    X['prev_event'] = X['prev_event'].map(mapper)
+    X['prev_active'] = X['prev_active'].map(mapper)
+    y = y.map(mapper)
+    Y = []
+    for each in y:
+        Y.append([each])
+    return X.to_numpy(), np.array(Y)
