a5a43a899b236703842ff038792583a06529e25b
AndyWeasley2004 <m5yao@ucsd.edu>
2023-11-08 10:52:55 -0800

    Graph modification

--- meals.py
+++ meals.py
@@ -72,7 +72,7 @@ def graph_pellet_frequency(grouped_data: pd.DataFrame, bhv, num):
     """graph histogram for pellet frequency
     histogram analysis
     """
-    ax = sns.barplot(data=grouped_data, x='Interval_Start', y='Pellet_Count', color='purple', alpha=0.5)
+    ax = sns.barplot(data=grouped_data, x='Interval_Start', y='Pellet_Count', color='#000099', alpha=0.5)
 
     # Get the x-axis positions
     xtick_positions = ax.get_xticks()
